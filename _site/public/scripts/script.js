2423857d561154cbab05c2de462eb209439a1c7f
Thiago Vieira <thiago@thiagovieira.com>
2012-07-30 14:15:00 -0300

    Melhorias Site Coderockr
    Floater para tablet android;

--- _site/public/scripts/script.js
+++ _site/public/scripts/script.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 	var interval=self.setInterval(function(){
 		if(window.pageYOffset <= 100)
 			$("header nav .home").parent().fadeOut(300);
-		else if(window.pageYOffset >= 300)
+		else if(window.pageYOffset >= 300 && parseInt($(document).width())>640)
 			$("header nav .home").parent().fadeIn(300);
 	},5);
 	$("#projects > nav a").not("#projects > nav.pages a").click(function(){
@@ -11,7 +11,10 @@ $(document).ready(function() {
 		$(this).removeClass("nonselect");
 	});
 	$("#projects > nav.pages ul").css({marginLeft:-(parseInt($("#projects > nav.pages ul").width()/2))});
-	$("#contact input[type=text], #contact textarea").focus(function(){
+	$("#contact label").bind('click',function(){
+		$(this).next().focus();
+	});
+	$("#contact input[type=text], #contact input[type=tel], #contact input[type=email], #contact textarea").focus(function(){
 		if($(this).val() == ""){
 			$(this).parent().find("label").fadeOut(150);
 		}		
@@ -28,7 +31,14 @@ $(document).ready(function() {
 		var href = $(this).attr("href");
 		href = href.replace("#","");
 		$('html, body').animate({scrollTop: $("a[name="+href+"]").offset().top}, 1000);
+		$("header nav").find(".active").removeClass("active");
+		$("header nav").find("a[href=#"+href+"]").addClass("active");
 	});
+
+	/*on start page*/
+	var hash = window.location.href.slice(window.location.href.indexOf('#') + 1);
+	if( hash != "" ) $("header nav").find("a[href=#"+hash+"]").addClass("active");
+
 	$("#contact-form")
 		.unbind('submit')
 		.bind('submit', function(){ 
