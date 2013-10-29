ca18cd7b308da5e71c80243c5f936d6d756a3d14
Thiago Vieira <thiago@thiagovieira.com>
2013-10-29 14:34:39 -0200

    Ajuste Idioma

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -112,7 +112,34 @@ $(document).ready(function() {
 			return false;
 		}
 	});
+	$('div[class=^"language-"] a').bind("click", function(){
+		var langValue = $(this).attr("rel");
+		createCookie("idioma", langValue, 60);
+	});
 });
+function createCookie(name, value, days) {
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        var expires = "; expires=" + date.toGMTString();
+    }
+    else var expires = "";
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
+function getCookie(c_name) {
+    if (document.cookie.length > 0) {
+        c_start = document.cookie.indexOf(c_name + "=");
+        if (c_start != -1) {
+            c_start = c_start + c_name.length + 1;
+            c_end = document.cookie.indexOf(";", c_start);
+            if (c_end == -1) {
+                c_end = document.cookie.length;
+            }
+            return unescape(document.cookie.substring(c_start, c_end));
+        }
+    }
+    return "";
+}
 function hideLanguage() {
 	$('input[name="language"]').attr('checked', false);
 }
