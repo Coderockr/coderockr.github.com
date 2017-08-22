8e442ecd434c2c9f298dc5ea0c68a3285f260a12
Raony Marcondes <raonymarcondes@gmail.com>
2017-08-22 15:41:48 -0300

    Update blog posts

--- static/app/scripts/main.js
+++ static/app/scripts/main.js
@@ -128,7 +128,20 @@ Main = {
   },
 
   initForm: function(){
-    $('[name=\'phone\']').mask('99 9999-9999?9',{placeholder:' '});
+    $('[name=\'phone\']')
+      .mask('99 9999-9999?9',{placeholder:' '})
+      .focusout(function (event) {
+      var target, phone, element;
+      target = (event.currentTarget) ? event.currentTarget : event.srcElement;
+      phone = target.value.replace(/\D/g, '');
+      element = $(target);
+      element.unmask();
+      if(phone.length > 10) {
+          element.mask('99 99999-999?9');
+      } else {
+          element.mask('99 9999-9999?9');
+      }
+  });
 
     var deadline = document.getElementById('deadline');
 
