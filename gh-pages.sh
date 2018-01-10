5dfdf0b165cd5417527ec9fcd64791b2b85d24c8
Raony Marcondes <raonymarcondes@gmail.com>
2018-01-10 16:05:14 -0200

    atualiza o script de deploy

--- gh-pages.sh
+++ gh-pages.sh
@@ -4,4 +4,6 @@ cp ./CNAME ./dist/CNAME
 cp ./humans.txt ./dist/humans.txt
 cp ./crossdomain.xml ./dist/crossdomain.xml
 cp ./README.md ./dist/README.md
-git subtree push --prefix dist origin master
+git subtree split --prefix dist -b master # create a local master branch containing the splitted output folder
+git push -f origin master:master # force the push of the master branch to the remote master branch at origin
+git branch -D master # delete the local master because you will need it: ref
