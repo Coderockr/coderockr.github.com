38e9d858ecbb3187af17d4cf0d4a5e91b707f751
Raony Marcondes <raonymarcondes@gmail.com>
2018-01-10 15:10:19 -0200

    atualização do site

---
+++ gh-pages.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+yarn build
+cp ./CNAME ./dist/CNAME
+cp ./humans.txt ./dist/humans.txt
+cp ./crossdomain.xml ./dist/crossdomain.xml
+cp ./README.md ./dist/README.md
+git subtree push --prefix dist origin master
---
+++ src/assets/scripts/globals.js
@@ -0,0 +1,7 @@
+import $ from 'jquery';
+
+window.jQuery = $;
+window.$ = window.jQuery;
+window.jquery = window.jQuery;
+
+export default $;
